4dc6e19fe96315ffdf67bdb776c508d0f21f7e44
Magvyy <MAHImprovement@gmail.com>
2022-09-20 00:55:51 +0200

    trold

--- Markus/Pandas_analysis/Analysis_Individual/StuffDistributionIndividualPiePlot.py
+++ Markus/Pandas_analysis/Analysis_Individual/StuffDistributionIndividualPiePlot.py
@@ -10,26 +10,26 @@ path_list = [folder_path + folder_name for folder_name in os.listdir(folder_path
 file_list = [[path + "/" + filename for filename in os.listdir(path)] for path in path_list]
 
 #Specifies data category to analyse and retrieves only those data files from above
-objects = ["electron", "jet", "photon", "tau"]
+stuffs = ["electron", "jet", "photon", "tau"]
 
 
 def specific_files(file_list):
-    objects_csv = [object + ".csv" for object in objects]
-    specific_file_list = [[file for object in objects_csv for file in files if file[-len(object):] == object] for files in file_list]
+    stuffs_csv = [stuff + ".csv" for stuff in stuffs]
+    specific_file_list = [[file for stuff in stuffs_csv for file in files if file[-len(stuff):] == stuff] for files in file_list]
     return specific_file_list
 
 
 #Takes list of datafiles from event type (i.e. background, blackhole etc) and returns a list of lists containing the datafiles pertaining to a specific object in objects_list
 #So of the form [[data1jet.csv, data2jet.csv], [data1electron.csv, data2electron.csv]... etc]
 def list_categorization(files):
-    categorized = [files[i*int(len(files)/len(objects)):(i + 1)*int(len(files)/len(objects))] for i in range(len(objects))]
+    categorized = [files[i*int(len(files)/len(stuffs)):(i + 1)*int(len(files)/len(stuffs))] for i in range(len(stuffs))]
     return categorized
 
 
 #Determines the shape of the subplot figure trying to be as square as possible
 #Should change it in case the amount of subplots is a prime number
 def plot_values(files):
-    plot_amount = int(len(files)/len(objects))
+    plot_amount = int(len(files)/len(stuffs))
     diff = 200
 
     for i in range(1, plot_amount + 1):
@@ -73,7 +73,7 @@ def plot_pie(data, relative_data, x_len, y_len, index_plot, title):
     plt.style.use(style)
     plt.subplot(x_len, y_len, index_plot + 1)
     plt.title(title)
-    labels = [object + " - " + str(data) + "%" for object, data in zip(objects, relative_data)]
+    labels = [object + " - " + str(data) + "%" for object, data in zip(stuffs, relative_data)]
     plt.pie(data, labels = labels)
     plt.grid()
 
@@ -84,11 +84,11 @@ def plot(file_list):
 
     for index_folder, files in enumerate(file_list):
         (x_len, y_len) = plot_values(files)
-        amount_per_category = int(len(files)/len(objects))
+        amount_per_category = int(len(files)/len(stuffs))
 
         for index_file in range(amount_per_category):        
             categorized = list_categorization(files)
-            title = subplot_title([os.path.basename(categorized[i][index_file]) for i in range(len(objects))])
+            title = subplot_title([os.path.basename(categorized[i][index_file]) for i in range(len(stuffs))])
             data, relative_data = data_pie(categorized, index_file)
             plot_pie(data, relative_data, x_len, y_len, index_file, title)
 

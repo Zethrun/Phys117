052d1ee88ca59225f7b36aa3c4269aab316e1d6d
Zethrun <113770426+Zethrun@users.noreply.github.com>
2022-09-26 21:57:58 +0200

    Submitted my files so far
    Updated the files to how i what i had done on the computer before i got git

--- Daniel/pandas__converter.py
+++ Daniel/pandas__converter.py
@@ -4,13 +4,17 @@ import pandas as pd
 import os
 
 #########
-background_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data/background"
-blackhole_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data/BH"
-sphalerion_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data/sphaleron"
+background_path = "data/data_lhco/background"
+blackhole_path = "data/data_lhco/BH"
+sphaleron_path = "data/data_lhco/sphaleron"
 
-new_background_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data_pandas/background"
-new_blackhole_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data_pandas/BH"
-new_sphaleron_path = "C:/Users/danie/OneDrive/Dokumenter/Studie/phys117/coding/data_pandas/sphaleron"
+new_sep_background_path = "data/data_pandas/gathered_seperated/background"
+new_sep_blackhole_path = "data/data_pandas/gathered_seperated/BH"
+new_sep_sphaleron_path = "data/data_pandas/gathered_seperated/sphaleron"
+
+new_norm_background_path = "data/data_pandas/normal/background"
+new_norm_blackhole_path = "data/data_pandas/normal/BH"
+new_norm_sphaleron_path = "data/data_pandas/normal/sphaleron"
 #########
 
 
@@ -18,10 +22,62 @@ columns = ["event", "eta", "phi", "PT", "jmass", "ntrk", "btag", "hadem"]
 particle_list = ["photon", "electron", "muon", "tau", "jet", "MET"]
 
 
-def convert_file_pandas(data_folder, output_folder, file):
-    pass
+def conv_singlefile(data_folder_path, output_folder_path, file):
 
-def sort_files_particles(data_folder_path, output_folder_path): #folder variable means folders path
+    count_dict = {
+        "event" : [],
+        "particle" : [],
+        "eta" : [],
+        "phi" : [],
+        "PT" : [],
+        "jmass" : [],
+        "ntrk" : [],
+        "btag" : [],
+        "hadem" : []
+    }
+
+    try:
+        events = LHCO_reader.Events(data_folder_path + "/" + file)
+        event_nr = 0
+        
+        for event in events:
+            for i in particle_list:
+                particles = event[i]
+                size = len(particles)
+                for j in range(size):
+                    particle = particles[j]
+
+                    count_dict["event"].append(event_nr)
+                    count_dict["particle"].append(i)
+                    count_dict["eta"].append(particle["eta"])
+                    count_dict["phi"].append(particle["phi"])
+                    count_dict["PT"].append(particle["PT"])
+                    count_dict["jmass"].append(particle["jmass"])
+                    count_dict["ntrk"].append(particle["ntrk"])
+                    count_dict["btag"].append(particle["btag"])
+                    count_dict["hadem"].append(particle["hadem"])
+
+
+            event_nr += 1 
+        
+    except:
+        print("")
+        print(f"Error: (Probably '{file}' has a wrong format.)")
+        print("List of file:")
+        print(file)
+        print("Is there a file too much with wrong format?")
+        print("")
+    
+    csv_file = pd.DataFrame(count_dict)
+    csv_file.to_csv(output_folder_path + "/" + file.removesuffix(".lhco") + ".csv")
+
+def conv_files_normally(data_folder_path, output_folder_path):
+    file_list = [filename for filename in os.listdir(data_folder_path)]
+
+    for file in file_list:
+        conv_singlefile(data_folder_path, output_folder_path, file)
+
+def conv_seperate_gather_particles(data_folder_path, output_folder_path): #folder variable means folders path
     file_list = [data_folder_path + "/" + filename for filename in os.listdir(data_folder_path)]
 
     count_dict = {
@@ -88,30 +144,41 @@ def sort_files_particles(data_folder_path, output_folder_path): #folder variable
     }
 
     for file in file_list:
-        events = LHCO_reader.Events(file)
-        event_nr = 0
+        try:
+            events = LHCO_reader.Events(file)
+            event_nr = 0
+            
+            for event in events:
+                for i in particle_list:
+                    particles = event[i]
+                    size = len(particles)
+                    for j in range(size):
+                        particle = particles[j]
+
+                        count_dict[i]["event"].append(event_nr)
+                        count_dict[i]["eta"].append(particle["eta"])
+                        count_dict[i]["phi"].append(particle["phi"])
+                        count_dict[i]["PT"].append(particle["PT"])
+                        count_dict[i]["jmass"].append(particle["jmass"])
+                        count_dict[i]["ntrk"].append(particle["ntrk"])
+                        count_dict[i]["btag"].append(particle["btag"])
+                        count_dict[i]["hadem"].append(particle["hadem"])
+
+
+                event_nr += 1 
         
-        for event in events:
-            for i in particle_list:
-                particles = event[i]
-                size = len(particles)
-                for j in range(size):
-                    particle = particles[j]
-
-                    count_dict[i]["event"].append(event_nr)
-                    count_dict[i]["eta"].append(particle["eta"])
-                    count_dict[i]["phi"].append(particle["phi"])
-                    count_dict[i]["PT"].append(particle["PT"])
-                    count_dict[i]["jmass"].append(particle["jmass"])
-                    count_dict[i]["ntrk"].append(particle["ntrk"])
-                    count_dict[i]["btag"].append(particle["btag"])
-                    count_dict[i]["hadem"].append(particle["hadem"])
-
-
-            event_nr += 1 
+        except:
+            print("")
+            print(f"Error: (Probably '{file}' has a wrong format.)")
+            print("List of file:")
+            print(file_list)
+            print("Is there a file too much with wrong format?")
+            print("")
 
     for particle in particle_list:
         csv_file = pd.DataFrame(count_dict[particle])
         csv_file.to_csv(output_folder_path + "/" + particle + ".csv")
 
-sort_files_particles(sphalerion_path, new_sphaleron_path)
+#conv_singlefile(background_path, new_norm_background_path, "ttbar.lhco")
+#conv_files_normally(sphaleron_path,new_norm_sphaleron_path)
+conv_seperate_gather_particles(background_path, new_sep_background_path)
